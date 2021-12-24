c307efecb42c3f704ba033f3eaeb27c1e40843a5
halfrot <loadinglyh@qq.com>
2021-12-24 11:43:36 +0800

    exp5

---
+++ exp5/Linux_mycp.cpp
@@ -0,0 +1,107 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <utime.h>
+using namespace std;
+const int BUFFSIZE = 1 << 10;
+void copyAttribute(string source, string target)
+{
+    struct stat StatBuf;
+    lstat(source.c_str(), &StatBuf);
+    chmod(target.c_str(), StatBuf.st_mode);
+    chown(target.c_str(), StatBuf.st_uid, StatBuf.st_gid);
+    if (S_ISLNK(StatBuf.st_mode))
+    {
+        struct timeval TimeSource[2];
+        TimeSource[0].tv_sec = StatBuf.st_mtime;
+        TimeSource[1].tv_sec = StatBuf.st_ctime;
+        lutimes(target.c_str(), TimeSource);
+    }
+    else
+    {
+        struct utimbuf TimeSource;
+        TimeSource.actime = StatBuf.st_atime;
+        TimeSource.modtime = StatBuf.st_mtime;
+        utime(target.c_str(), &TimeSource);
+    }
+}
+void copyNormalFile(string source, string target)
+{
+    int FCBSource = open(source.c_str(), O_RDONLY);
+    int FCBTarget = creat(target.c_str(), O_WRONLY);
+    char buf[BUFFSIZE];
+    int tot = 0;
+    while ((tot = read(FCBSource, buf, BUFFSIZE)) > 0)
+        write(FCBTarget, buf, tot);
+    close(FCBSource);
+    close(FCBTarget);
+    copyAttribute(source, target);
+}
+void copyLink(string source, string target)
+{
+    char PathSource[BUFFSIZE];
+    readlink(source.c_str(), PathSource, BUFFSIZE);
+    symlink(PathSource, target.c_str());
+    copyAttribute(source, target);
+}
+void copyDir(string source, string target)
+{
+    DIR *PDSource = opendir(source.c_str());
+    struct dirent *EntrySource = NULL;
+    struct stat StatBuf;
+    lstat(source.c_str(), &StatBuf);
+    if (opendir(target.c_str()) == NULL)
+        mkdir(target.c_str(), StatBuf.st_mode);
+    while (EntrySource = readdir(PDSource))
+    {
+        if (string(EntrySource->d_name) == ".." || string(EntrySource->d_name) == ".")
+            continue;
+        cout << "-----copy " + string(EntrySource->d_name) + "-----" << endl;
+        string NextSource = source + "/" + EntrySource->d_name;
+        string NextTarget = target + "/" + EntrySource->d_name;
+        lstat(NextSource.c_str(), &StatBuf);
+        if (S_ISDIR(StatBuf.st_mode))
+            copyDir(NextSource, NextTarget);
+        else if (S_ISREG(StatBuf.st_mode))
+            copyNormalFile(NextSource, NextTarget);
+        else if (S_ISLNK(StatBuf.st_mode))
+            copyLink(NextSource, NextTarget);
+    }
+    cout << "*****" << endl
+         << "directory " + source + " end" << endl
+         << "*****" << endl;
+    copyAttribute(source, target);
+}
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        cout << "-----argument error-----" << endl;
+        return 0;
+    }
+    struct stat StatBuf;
+    string source(argv[1]), target(argv[2]);
+    if (lstat(argv[1], &StatBuf) == -1)
+    {
+        cout << "-----source dir error-----" << endl;
+        return 0;
+    }
+    if (S_ISDIR(StatBuf.st_mode))
+    {
+        if (opendir(argv[1]) == NULL)
+        {
+            cout << "-----source dir error-----" << endl;
+            return 0;
+        }
+        copyDir(source, target);
+    }
+    return 0;
+}
---
+++ exp5/Windows_mycp.cpp
@@ -0,0 +1,68 @@
+﻿#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<Windows.h>
+#include<string>
+#include<iostream>
+using namespace std;
+const int BUFFSIZE = 1 << 10;
+void copyAttribute(string source, string target) {
+	DWORD SourceFileAttribute = GetFileAttributes(source.c_str());
+	SetFileAttributes(target.c_str(), SourceFileAttribute);
+	HANDLE HSource = CreateFile(source.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL,
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_BACKUP_SEMANTICS, NULL);
+	HANDLE HTarget = CreateFile(target.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL,
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_BACKUP_SEMANTICS, NULL);
+	FILETIME SourceFileCreate, SourceFileAccess, SourceFileLastWrite;
+	GetFileTime(HSource, &SourceFileCreate, &SourceFileAccess, &SourceFileLastWrite);
+	SetFileTime(HTarget, &SourceFileCreate, &SourceFileAccess, &SourceFileLastWrite);
+}
+void copyNormalFile(string source, string target) {
+	HANDLE HSource = CreateFile(source.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL,
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_BACKUP_SEMANTICS, NULL);
+	HANDLE HTarget = CreateFile(target.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL,
+		CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_BACKUP_SEMANTICS, NULL);
+	if (HTarget == INVALID_HANDLE_VALUE) {
+		cout << "-----target file error-----" << endl;
+		return;
+	}
+	char buf[BUFFSIZE];
+	DWORD tot = BUFFSIZE;
+	while (tot == BUFFSIZE && ReadFile(HSource, buf, BUFFSIZE, &tot, NULL)) WriteFile(HTarget, buf, tot, &tot, NULL);
+	CloseHandle(HSource);
+	CloseHandle(HTarget);
+	copyAttribute(source, target);
+}
+void copyDir(string source, string target) {
+	WIN32_FIND_DATA FindData;
+	if (FindFirstFile((target + "\\*.*").c_str(), &FindData) == INVALID_HANDLE_VALUE) CreateDirectory(target.c_str(), NULL);
+	HANDLE HFile = FindFirstFile((source + "\\*.*").c_str(), &FindData);
+	while (FindNextFile(HFile, &FindData)) {
+		if (string(FindData.cFileName) == ".." || string(FindData.cFileName) == ".") continue;
+		cout << "-----copy " + string(FindData.cFileName) + "-----" << endl;
+		string NextSource = source + "\\" + FindData.cFileName;
+		string NextTarget = target + "\\" + FindData.cFileName;
+		if (FindData.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY) copyDir(NextSource, NextTarget);
+		else copyNormalFile(NextSource, NextTarget);
+	}
+	FindClose(HFile);
+	cout << "*****" << endl
+		<< "directory " + source + " end" << endl
+		<< "*****" << endl;
+	copyAttribute(source, target);
+}
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		cout << "-----argument error-----" << endl;
+		return 0;
+	}
+	WIN32_FIND_DATA FindData;
+	string source(argv[1]), target(argv[2]);
+	if (FindFirstFile((source + "\\*.*").c_str(), &FindData) == INVALID_HANDLE_VALUE) {
+		cout << "-----source dir error-----" << endl;
+		return 0;
+	}
+	if (FindData.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY) copyDir(source, target);
+	else cout << "-----source dir error-----" << endl;
+	return 0;
+}
